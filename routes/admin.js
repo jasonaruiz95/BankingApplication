3f84d75da6d5181b7d76c69d8d703165364d7196
jasonaruiz95 <jasonruiz1@mail.weber.edu>
2023-03-04 21:51:53 -0700

    Added functionality for changing user roles

--- routes/admin.js
+++ routes/admin.js
@@ -12,32 +12,46 @@ function renderRoleChangePage(req, res) {
     //Need to submit role change to db upon second post. Second post will have a ProposedType value.
     objForRoleChange = {};
     const username = req.body.search;
-    let name;
-    let user_id;
-    let role;
-    let proposedRole;
+    const proposedRole = req.body.purposedRole;
+    const user_id = req.body.user_id;
+    const role = req.body.role;
+
 
     step1(res);
    //Get user account that matches search bar
+  
   function step1(res) {
+    if(username !== undefined || username == ""){
     let sql = "CALL check_user(?);"
     dbCon.query(sql, [username], function(err, rows) {
       if (err){
         throw err;
         }
       console.log("admin.js: Obtained check_credentials reply from database");
-      if (rows[0][0] === undefined || rows[0][0].result == 0) {
-        console.log("admin.js: No login credentials were found");
-        // objForUsersPage.message = "username: '" + username + "' is not valid, please try again.";
-        // renderIt(res);
-        res.render('admin', {message: "username: '" + username + "' is not valid, please try again."});
-        }
-      else {
-        console.log("admin.js: Credentials matched");
-        step2(res);
-        }
-      });
-      };
+        if (rows[0][0] === undefined || rows[0][0].result == 0) {
+          console.log("admin.js: No login credentials were found");
+          // objForUsersPage.message = "username: '" + username + "' is not valid, please try again.";
+          // renderIt(res);
+          res.render('admin', {
+            message: "username: '" + username + "' is not valid, please try again.",
+          });
+          }
+        else {
+          console.log("admin.js: Credentials matched");
+          step2(res);
+          }
+        });
+      }
+      else{
+
+      console.log("admin.js: Role Change request detected.")
+      console.log("username: " + username + "| user_id: " + user_id + "| role: " + role);
+
+
+    }
+  }
+  //Else if username is undefined
+  
       
   function step2(res){
     let sql = "CALL get_role_info(?)";
@@ -46,8 +60,10 @@ function renderRoleChangePage(req, res) {
         throw err
       }
       console.log("admin.js: Getting role info for username");
+      console.log("admin.js: Username: " + username);
       objForRoleChange.roleInfo = rows[0];
-      res.render(admin, objForRoleChange);
+      console.log(rows);
+      res.render('admin', objForRoleChange);
     }
     );
   }
--- routes/adminRoles.js
+++ routes/adminRoles.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var dbCon = require("../lib/database");
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('adminRoles');
+});
+
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  const user_id = req.body.postUser_ID;
+  const proposedRole = req.body.proposedRole;
+
+  console.log("This is proposed Role: " + proposedRole + " This is user id: " + user_id);
+  if(user_id){
+  let sql = "CALL change_user_type(?,?)"
+  dbCon.query(sql, [user_id, proposedRole], function(err, rows){
+    if (err) {
+      throw err;
+    }
+
+    console.log("adminRoles.js: Change user request submitted")
+    message = "Users role has been changed to: " + proposedRole;
+    res.render('adminRoles', {message: "Users role has been changed to: " + proposedRole});
+
+  });
+  }
+  else{
+    res.render('adminRoles');
+  }
 });
 
 module.exports = router;
